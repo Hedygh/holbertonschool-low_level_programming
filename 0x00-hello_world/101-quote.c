3152e7c6fa6f11d45be1f2f4334b698db4bd17f0
hedygh <dyouahes@gmail.com>
2021-02-03 14:42:34 +0100

    hello_world_practice

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - print string to stderr using fprintf
+ * Description: Using fprintf specifying the destination to stderr
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
