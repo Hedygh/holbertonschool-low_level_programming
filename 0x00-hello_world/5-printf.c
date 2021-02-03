45190f8a1f38fc509189a95ea7266e6314966fea
hedygh <dyouahes@gmail.com>
2021-02-03 14:24:17 +0100

    hello_world_practice

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * main - printf to print string
  * Description: main program using printf
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
