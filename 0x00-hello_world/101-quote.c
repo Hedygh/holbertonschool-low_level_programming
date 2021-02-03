6897a721dd1922249e69c5e35c7ae07c24f711f7
hedygh <dyouahes@gmail.com>
2021-02-03 15:19:56 +0100

    using_write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - print string to stderr using fprintf
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-\
-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
