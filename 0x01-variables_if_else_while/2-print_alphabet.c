9ee202b25975f3df9e514a267c2f42409cdcba32
hedygh <dyouahes@gmail.com>
2021-02-04 12:52:48 +0100

    Variables_statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - using putchar
+ * Description: looping through putchar to print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+	char z = 'z';
+
+	while (a < z)
+	{
+		putchar(a++);
+	}
+	putchar('\n');
+	return (0);
+}
