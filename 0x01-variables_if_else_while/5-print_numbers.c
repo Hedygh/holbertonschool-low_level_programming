077f048eb6ab9da840e2ccb738693df9c6902bd3
hedygh <dyouahes@gmail.com>
2021-02-04 14:26:12 +0100

    Variables_statements

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - alphabet
+ * Description: print numbers
+ * Return: 0
+ */
+int main(void)
+{
+	char a = '0';
+
+	while (a <= '9')
+		putchar(a++);
+			putchar('\n');
+	return (0);
+}
