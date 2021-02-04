ae739d346d61ccacc144e9de2e79fe346c97cd5a
hedygh <dyouahes@gmail.com>
2021-02-04 09:03:48 +0100

    rm wspace

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
 #include<unistd.h>
-
 /**
- * main - print string 
+ * main - print string
  * Description: Using write specifying the number of char
  * Return: 1
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
