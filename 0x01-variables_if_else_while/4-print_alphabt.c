f4c3d921f52d24fdd9e3dab38837890b243d7ac3
hedygh <dyouahes@gmail.com>
2021-02-04 14:09:55 +0100

    Variables_statements

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ * Description: print alphabet without e and q
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+	char z = 'z';
+
+	while (a < z)
+	{
+		if (a != 'q' && a != 'e')
+			putchar(a);
+		a++;
+	}
+	return (0);
+}
