ecfcfdced0bc033b29e6e5295a7da9e261777e73
hedygh <dyouahes@gmail.com>
2021-02-09 19:35:51 +0100

    100times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,7 +27,7 @@ void print_times_table(int n)
 	int j;
 	int calc;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
 		while (i <= n)
 		{
@@ -53,4 +53,15 @@ void print_times_table(int n)
 			i++;
 		}
 	}
+}
+int main(void)
+{
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
 }
