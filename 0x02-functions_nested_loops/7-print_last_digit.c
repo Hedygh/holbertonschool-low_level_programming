ba2a81d2e4b08a4c96b4c6edc4f8e394e1a74a25
hedygh <dyouahes@gmail.com>
2021-02-09 18:57:25 +0100

    last dig

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,12 +13,11 @@ int print_last_digit(int i)
 		i = -1 * i;
 		j = i % 10;
 		_putchar(j + '0');
-		return (j);
 	}
 	else
 	{
 		j = i % 10;
 		_putchar(j + '0');
-		return (j);
 	}
+	return (j);
 }
