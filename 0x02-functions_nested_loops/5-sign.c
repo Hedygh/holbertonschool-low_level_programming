38c629745b20cc138448660c184a7bbb0f3bf897
hedygh <dyouahes@gmail.com>
2021-02-09 18:44:06 +0100

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,10 +6,10 @@
  */
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
@@ -18,33 +18,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar('+');
-		return (1);
+		_putchar('-');
+		return (-1);
 	}
-}
-int main(void)
-{
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
 }
