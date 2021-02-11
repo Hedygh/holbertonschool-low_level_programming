cc10eb00a3b87670d026320897c6da68aac866bb
hedygh <dyouahes@gmail.com>
2021-02-11 19:35:06 +0100

    primefactor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - find and print the largest prime factor of number
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int c = 2;
+	long int num = 612852475143;
+
+	while (num > 1)
+	{
+		while (num % c == 0)
+		{
+			num = num / c;
+		}
+		c++;
+	}
+	printf("%d\n", c);
+	return (0);
+}
