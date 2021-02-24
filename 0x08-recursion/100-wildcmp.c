7d04a5c99a515669eefa382f63b873feafe50384
hedygh <dyouahes@gmail.com>
2021-02-24 23:14:53 +0100

    wildcmp

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -15,6 +15,8 @@ int is(char *s1, char *s2, int i, int j)
 
 	if (s2[j] == s1[i])
 		return (is(s1, s2, i + 1, j + 1));
+	if (s2[j] == '*' && s2[j + 1] != '\0' && s1[i] == '\0')
+		return (0);
 	if (s2[j] == '*')
 		return ((is(s1, s2, i, j + 1)) || (is(s1, s2, i + 1, j)));
 	return (0);
