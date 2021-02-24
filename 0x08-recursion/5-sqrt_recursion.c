41bacd4c060e31fc4549fa8da518e35d47181b92
hedygh <dyouahes@gmail.com>
2021-02-24 14:38:53 +0100

    add return

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -27,21 +27,3 @@ int _sqrt_recursion(int n)
 		return (1);
 	return (_sqrt2(n, 1));
 }
-int main(void)
-{
-    int r;
-
-    r = _sqrt_recursion(1);
-    printf("%d\n", r);
-    r = _sqrt_recursion(1024);
-    printf("%d\n", r);
-    r = _sqrt_recursion(16);
-    printf("%d\n", r);
-    r = _sqrt_recursion(17);
-    printf("%d\n", r);
-    r = _sqrt_recursion(25);
-    printf("%d\n", r);
-    r = _sqrt_recursion(-1);
-    printf("%d\n", r);
-    return (0);
-}
