b12515084df3c3f6d21030345688cacc290a9d25
hedygh <dyouahes@gmail.com>
2021-02-24 22:39:10 +0100

    wildcmp

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -30,7 +30,7 @@ int is(char *s1, char *s2, int i, int j)
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
 	return (is(s1, s2, 0, 0));
 }
