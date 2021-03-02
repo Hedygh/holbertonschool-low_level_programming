536714310ad9a05f8a21c8d27eb62902f64424b8
hedygh <dyouahes@gmail.com>
2021-03-02 14:57:21 +0100

    syntax

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,29 +4,29 @@
 
 /**
  * alloc_grid - allocate memory for 2d array of int
- * @w: columns
- * @h: rows
+ * @width: columns
+ * @height: rows
  * Return: pointer to 2d arrays created
  */
-int **alloc_grid(int w, int h)
+int **alloc_grid(int width, int height)
 {
 	int **tab;
 	int i = 0;
 	int j = 0;
 
-	tab = malloc(sizeof(int *) * h);
+	tab = malloc(sizeof(int *) * height);
 	if (!tab)
 		return (0);
-	while (i < h)
+	while (i < height)
 	{
-		tab[i] = malloc(sizeof(int) * w);
+		tab[i] = malloc(sizeof(int) * width);
 		i++;
 	}
 	i = 0;
-	while (i < h)
+	while (i < height)
 	{
 		j = 0;
-		while (j < w)
+		while (j < width)
 		{
 			tab[i][j] = 0;
 			j++;
