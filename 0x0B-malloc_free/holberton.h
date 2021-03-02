a71d276ae55312b5292fa9db4b9027b5e33a00ba
hedygh <dyouahes@gmail.com>
2021-03-02 21:41:48 +0100

    strtow

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -5,6 +5,13 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+void _free_grid(int **grid, int height);
 void free_grid(int **grid, int height);
 int _strlen(char *str);
+char *argstostr(int ac, char **av);
+char *_strcat(char *dest, char *src);
+char **strtow(char *str);
+int ft_countword(char *str);
+int ft_checkspace(char *str, int i);
+char *_strndup(char *str, int n);
 #endif
