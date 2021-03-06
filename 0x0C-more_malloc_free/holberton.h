e4b659d04e5add78bd8fce766b99613fe423a1f6
hedygh <dyouahes@gmail.com>
2021-03-06 21:15:42 +0100

    mul

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -4,6 +4,10 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-unsigned int _strlen(char *s);
+int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *mul(char *s1, char *s2);
+char *revandzero(int *tab, int i, int j, int length, char *str, int digit);
+int _isdigit(int c);
 #endif
