ee15a0d8d539d4398f78f5bc19045c2b0d20ed92
hedygh <dyouahes@gmail.com>
2021-03-10 14:01:27 +0100

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - point to cmp func for every element of array
+ * @array: array to check
+ * @size: size of the array
+ * @cmp: pointer to function checks
+ * Return: index of match or -1 if error or no match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
