51201d804da56f4bcf4e2f29ca074f77b9a6f617
hedygh <dyouahes@gmail.com>
2021-03-10 18:46:35 +0100

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,28 +9,28 @@
  */
 int main(int ac, char **av)
 {
-	if (ac == 4)
+	if (ac != 4)
 	{
-		int i = atoi(av[1]);
-		int j = atoi(av[3]);
-
-		if ((j == 0) && (av[2][0] == '/' || av[2][0] == '%'))
-		{
-			printf("Error\n");
-			exit(100);
-		}
-		if (av[2][0] != '%' && av[2][0] != '+' && av[2][0] != '/' &&
-				av[2][0] != '-' && av[2][0] != '*')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		int (*res)(int, int);
-
-		res = get_op_func(av[2]);
-		printf("%d\n", res(i, j));
-		return (0);
+		printf("Error\n");
+		exit(98);
 	}
-	printf("Error\n");
-	exit(98);
+	int i = atoi(av[1]);
+	int j = atoi(av[3]);
+	int (*res)(int, int);
+
+	if ((j == 0) && (av[2][0] == '/' || av[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (av[2][0] != '%' && av[2][0] != '+' && av[2][0] != '/' &&
+			av[2][0] != '-' && av[2][0] != '*')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	res = get_op_func(av[2]);
+	printf("%d\n", res(i, j));
+	return (0);
 }
