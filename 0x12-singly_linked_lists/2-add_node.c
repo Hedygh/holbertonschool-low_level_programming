7e971053137538710b605ec0d62a0baabdff06a4
hedygh <dyouahes@gmail.com>
2021-03-18 19:50:57 +0100

    check

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
 #include <string.h>
+#include <stddef.h>
 /**
  * _strlen - length of string
  * @str: string
  * Return: length
  */
-int _strlen(char *str)
+int _strlen(const char *str)
 {
 	int i = 0;
 
@@ -24,14 +25,16 @@ int _strlen(char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode;
+	int i = 0;
 
 	if (head == NULL)
 		return (NULL);
 	newnode = malloc(sizeof(list_t));
 	if (!newnode)
 		return (NULL);
+	i = _strlen(str);
 	newnode->str = strdup(str);
-	newnode->len = strlen(newnode->str);
+	newnode->len = i;
 	newnode->next = (*head);
 	(*head) = newnode;
 	return (newnode);
