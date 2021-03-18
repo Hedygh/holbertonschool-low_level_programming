bac2fa542a61e1ea999b7651bba21fd05f755c6d
hedygh <dyouahes@gmail.com>
2021-03-18 18:32:23 +0100

    task 0 print list

---
+++ 0x012-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * print_list - print all element of a linked list
+ * @h: list to print from
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
