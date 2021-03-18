45d66296cc0cb1153a6bdafca26c2e9b9772761a
hedygh <dyouahes@gmail.com>
2021-03-18 21:41:00 +0100

    Not clear yet

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .rodata
+format: db 'Hello, %s', 10, 0
+name: db 'Holberton', 0
+section .text
+global main
+extern printf
+main:
+push rbp
+mov rdi, format
+mov rsi, message
+mov rax, 0
+call printf
+mov rax, 0
+ret
