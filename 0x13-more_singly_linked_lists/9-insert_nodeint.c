c0545024d936da52886f3d071b98c557fa109904
hedygh <dyouahes@gmail.com>
2021-03-23 02:53:36 +0100

    check for 0 index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!newnode)
 		return (NULL);
 	newnode->n = n;
-	newnode->next = headtmp->next;
-	headtmp->next = newnode;
+	if (idx == 0)
+	{
+		newnode->next = (*head);
+		(*head) = newnode;
+		return (newnode);
+	}
+	else
+	{
+		newnode->next = headtmp->next;
+		headtmp->next = newnode;
+	}
 	return (newnode);
 }
