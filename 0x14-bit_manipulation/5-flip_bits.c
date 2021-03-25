2c2d97a70a3f04be49eb3c1daa3764e46695bf24
hedygh <dyouahes@gmail.com>
2021-03-25 22:06:21 +0100

    flip_bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * count_bit - count number of 1 bits
+ * @res: number to check
+ * Return: result of check
+ */
+unsigned int count_bit(unsigned int res)
+{
+	if (res <= 0)
+		return (0);
+	if (res & 1)
+		return (count_bit(res >> 1) + 1);
+	else
+		return (count_bit(res >> 1));
+}
+/**
+ * flip_bits - find numbers of bits to flip to have same num
+ * @n: number 1
+ * @m: number 2
+ * Return: number of diff
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int res = 0;
+
+	res = n ^ m;
+
+	return (count_bit(res));
+}
