2ed3f574d7e13d15fed922f1cbd32bf0aedbcc9f
hedygh <dyouahes@gmail.com>
2021-03-30 15:59:54 +0200

    betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,27 +16,26 @@ void cpy_ffrom_fto(char *from, char *to)
 
 	fd_from = open(from, O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from %s\n", from);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from %s\n", from),
+			exit(98);
 	fd_to = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", to);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't write to %s\n", to),
+			exit(98);
 	rf = read(fd_from, buff, 1024);
 	if (rf == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", from);
 		exit(98);
 	}
-	wt = write(fd_to, buff, rf);
-	if (wt == -1)
+	while (rf != 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", to);
-		exit(99);
+		wt = write(fd_to, buff, rf);
+		if (wt == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", to);
+			exit(99);
+		}
 	}
 	cl = close(fd_from);
 	if (cl == -1)
