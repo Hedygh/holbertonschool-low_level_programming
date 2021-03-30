135bf67e037b2635318312ab1f244ef86de325b5
hedygh <dyouahes@gmail.com>
2021-03-30 16:11:50 +0200

    new close func

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,4 +5,5 @@ int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 void cpy_ffrom_fto(char *from, char *to);
+void closefile(int fd);
 #endif
