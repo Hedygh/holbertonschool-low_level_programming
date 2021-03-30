33a5d37dfc9ccbdbfefe847ee19e5d65b78af356
hedygh <dyouahes@gmail.com>
2021-03-30 12:33:27 +0200

    case -1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buff)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (!fd)
+	if (fd == -1)
 	{
 		free(buff);
 		return (0);
