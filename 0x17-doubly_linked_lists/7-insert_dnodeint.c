d5f96f7169e6f775665f9d402aba6189a78f80ca
hedygh <dyouahes@gmail.com>
2021-05-02 21:42:38 +0200

    bad swap

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,31 +8,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *tmp;
+	dlistint_t *new, *tmp = *h;
 	unsigned int i = 0;
-
-	if (!(*h))
-		return (NULL);
-	tmp = (*h);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+	while (i < idx - 1)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+		i++;
+	}
 	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
-	while (tmp != NULL && i < idx - 1)
-	{
-		tmp = tmp->next;
-		i++;
-	}
 	if (tmp != NULL)
 	{
 		new->n = n;
 		new->next = tmp->next;
 		new->prev = tmp;
+		tmp->next->prev = new;
 		tmp->next = new;
 	}
 	return (new);
