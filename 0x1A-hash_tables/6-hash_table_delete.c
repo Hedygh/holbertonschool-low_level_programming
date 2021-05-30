d50deab3ca444e7fef030a709ae13845cd2b84f1
hedygh <dyouahes@gmail.com>
2021-05-30 16:10:32 +0200

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes hash table
+ * @ht: hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *tmp;
+
+	i = 0;
+	while (i < ht->size)
+	{
+		while (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = tmp;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
