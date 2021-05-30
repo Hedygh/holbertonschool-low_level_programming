08c6eaef2f0cb4f9306f5193f1c50cbfa8c867e0
hedygh <dyouahes@gmail.com>
2021-05-30 12:57:47 +0200

    size condition

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hsh_table = NULL;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 
 	hsh_table = malloc(sizeof(hash_table_t));
