aa83fad02e656db92c6b499f9aa7247009a08df7
hedygh <dyouahes@gmail.com>
2021-05-30 13:13:01 +0200

    value to each member of array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,9 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hsh_table;
+	unsigned int i;
 
+	i = 0;
 	if (size == 0)
 		return (NULL);
 
@@ -26,5 +28,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	hsh_table->size = size;
+
+	while (i < size)
+	{
+		hsh_table->array[i] = NULL;
+		i++;
+	}
 	return (hsh_table);
 }
