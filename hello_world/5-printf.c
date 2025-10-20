a6a1f835cb6e45c60f9d008d806c651f06290aa5
Hedygh <dyouahes@gmail.com>
2025-10-20 14:07:39 +0200

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
