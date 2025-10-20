5e2017dd922e8860488027422dd80d7dbbf8130c
Hedygh <dyouahes@gmail.com>
2025-10-21 01:05:19 +0200

    comb3

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Description: Print all combinations of 2 digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '1';
+
+	while (i <= '8')
+	{
+		while (j <= '9')
+		{
+			putchar(i);
+			putchar(j);
+			if (i < '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
