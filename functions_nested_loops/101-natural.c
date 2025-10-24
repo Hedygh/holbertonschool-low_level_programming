5eba3a9f0c40eb7ad261b4fd3af25d9c59235f5f
Hedygh <dyouahes@gmail.com>
2025-10-24 13:09:49 +0200

    101

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_num_recursive - print num of any lenght
+ * @n: int to print
+ * Return: none
+ */
+
+void print_num_recursive(int n)
+{
+	if (n >= 10)
+		print_num_recursive(n / 10);
+	_putchar(n % 10 + '0');
+}
+/**
+ * main - Entry
+ * Description: sum of all multiple of 3/5 under 1024
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 1024;
+	int sum = 0;
+
+	while (i > 0)
+	{
+		if ((i % 5 == 0) || (i % 3 == 0))
+			sum = i + sum;
+		i--;
+	}
+	print_num_recursive(sum);
+	return (0);
+}
