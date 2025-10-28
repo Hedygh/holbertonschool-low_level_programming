2ac923ab7dc957131a3be5445236a07d338406d2
Hedygh <dyouahes@gmail.com>
2025-10-28 15:32:11 +0100

    101

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,18 @@
  */
 void print_number(int n)
 {
+	unsigned int x = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		x = -x;
 	}
-	if (n >= 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (x <= 9)
+		_putchar(x + '0');
+	if (x >= 10)
+	{
+		print_number(x / 10);
+		_putchar(x % 10 + '0');
+	}
 }
