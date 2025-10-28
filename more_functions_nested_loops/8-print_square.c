d85e59075b7f4ae9ba883ca9563502b077e1f815
Hedygh <dyouahes@gmail.com>
2025-10-28 12:27:38 +0100

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - square
+ * @size: numbers of line
+ * Return: none
+ */
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	if (size > 0)
+	{
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+		_putchar('\n');
+}
