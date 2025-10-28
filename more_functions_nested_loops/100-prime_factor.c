4f93ffad55abf695886dea2c4444e27e6b31f2f8
Hedygh <dyouahes@gmail.com>
2025-10-28 21:23:41 +0100

    task 100

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -7,5 +7,17 @@
  */
 int main(void)
 {
+	long int i = 1;
+	long int m = 612852475143;
 
+	while (m > 1)
+	{
+		i++;
+		while (m % i == 0)
+		{
+			m = m / i;
+		}
+	}
+	printf("%ld", i);
+	return (0);
 }
