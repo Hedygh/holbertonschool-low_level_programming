5cc7fd5ada37d4f5f0334fc6cf84c1da3889715f
Hedygh <dyouahes@gmail.com>
2025-10-29 15:03:12 +0100

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strlen - lenght of string
+ * @s: lenght
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * puts_half - print half of string
+ * @str: string
+ * Return: none
+ */
+void puts_half(char *str)
+{
+	int l = _strlen(str) + 1;
+	int h = l / 2;
+
+	while (h <= l)
+	{
+		_putchar(str[h]);
+		h++;
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ void print_rev(char *s);
 void rev_string(char *s);
 int _putchar(char c);
 void puts2(char *str);
+void puts_half(char *str);
