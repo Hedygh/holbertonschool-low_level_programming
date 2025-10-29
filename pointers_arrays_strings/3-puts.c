5f0ea7561a03e6def07fe6be91946cc433af873a
Hedygh <dyouahes@gmail.com>
2025-10-29 12:02:46 +0100

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - print string
+ * @str: string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		_putchar(str[i++]);
+}
