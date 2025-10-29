6fafae8e7d42e5f7d67449db03e88d84acae9d80
Hedygh <dyouahes@gmail.com>
2025-10-29 15:35:41 +0100

    simplify

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,16 +12,12 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	if (n > 0)
-	{	
-		while (i < n)
-		{
-			if (i < n - 1)
-				printf("%d, ", a[i]);
-			else
-				printf("%d", a[i]);
-			i++;
-		}
-		printf("\n");
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+		i++;
 	}
+	printf("\n");
 }
