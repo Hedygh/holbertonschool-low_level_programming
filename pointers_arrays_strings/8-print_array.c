8beac1e70ba778cc3c27fe4556e7b6ecfe07896e
Hedygh <dyouahes@gmail.com>
2025-10-29 15:31:05 +0100

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,15 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i < n)
-	{
-		if (i < n - 1)
-			printf("%d, ", a[i]);
-		else
-			printf("%d\n", a[i]);
-		i++;
+	if (n > 0)
+	{	
+		while (i < n)
+		{
+			if (i < n - 1)
+				printf("%d, ", a[i]);
+			else
+				printf("%d\n", a[i]);
+			i++;
+		}
 	}
 }
