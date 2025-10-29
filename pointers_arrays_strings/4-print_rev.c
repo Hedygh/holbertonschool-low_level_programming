7390aaf91416a722fa1431d7bc5bae9e0cea3fe8
Hedygh <dyouahes@gmail.com>
2025-10-29 13:46:17 +0100

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse string
+ * @s: string to rev
+ * Return: none
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i])
+		i++;
+	while (j <= i)
+	{
+		_putchar(s[i]);
+			i--;
+	}
+	_putchar(10);
+}
