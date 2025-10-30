5e09e3f03738d36d7a74ed5f7e17e59a34092312
Hedygh <dyouahes@gmail.com>
2025-10-30 13:44:48 +0100

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - lenght of string
+ * @s: lenght
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
  * _strncpy - copy n char to dest
  * @dest: receptor
  * @src: source to copy from
@@ -12,11 +28,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] && i < n)
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	while (i < n)
+	{
 	dest[i] = '\0';
+	i++;
+	}
 	return (dest);
 }
