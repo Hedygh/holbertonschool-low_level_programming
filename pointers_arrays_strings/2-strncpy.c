b65261d6ffc0408519694858bbe28e1cefbc0863
Hedygh <dyouahes@gmail.com>
2025-10-30 13:33:33 +0100

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy n char to dest
+ * @dest: receptor
+ * @src: source to copy from
+ * @n: copy n char
+ * Return: copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
