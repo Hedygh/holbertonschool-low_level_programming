84f9c2e593a34cf3675209a3b076bbe3f82127be
Hedygh <dyouahes@gmail.com>
2025-10-30 18:57:52 +0100

    add -

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -16,11 +16,12 @@ char *cap_string(char *str)
 		while ((str[i] >= 32 && str[i] <= 47) ||
 				(str[i] >= 123 && str[i] < 127))
 			i++;
-		if ((str[i] >= 'a' && str[i] <= 'z') 
+		if ((str[i] >= 'a' && str[i] <= 'z')
 				&& (!(str[i - 1] >= 'A') && (str[i - 1] <= 'Z')) &&
-				(!(str[i - 1] >= '0' && str[i -1] <= '9')))
+				(!(str[i - 1] >= '0' && str[i - 1] <= '9')) &&
+				(!(str[i - 1] == '-')))
 			str[i] -= 32;
 		i++;
-	}	
+	}
 	return (str);
 }
