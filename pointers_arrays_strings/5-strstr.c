79203892c69a2a35add846d97253248b9e459c36
Hedygh <dyouahes@gmail.com>
2025-11-03 14:51:28 +0100

    5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - find needle in hays
+ * @haystack: string to search
+ * @needle: string to find
+ * Return: first occurence of needle
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	while (haystack[i])
+	{
+		j = 0;
+		if (needle[0] == haystack[i])
+			while (haystack[i + j] == needle[j] && needle[j])
+			{
+				if (needle[j + 1] == '\0')
+					return (&haystack[i]);
+				j++;
+			}
+		i++;
+	}
+	return (0);
+}
