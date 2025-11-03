11a012b9fb629a034e41e6ab969304e887ee7d8a
Hedygh <dyouahes@gmail.com>
2025-11-03 21:53:05 +0100

    7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: board to print
+ *
+ * Return: none
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
