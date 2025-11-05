e30cc47e4ee2a16748c647b2bb28c9470f8ed8b1
Hedygh <dyouahes@gmail.com>
2025-11-05 15:57:40 +0100

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - Entry
+ * @ac: argc
+ * @av: argv
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	(void)ac;
+
+	printf("%s\n", av[0]);
+	return (0);
+}
