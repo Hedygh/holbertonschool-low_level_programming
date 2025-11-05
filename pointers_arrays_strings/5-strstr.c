fd4003c44b26ac3d26168d32a5f704ac5cbaf42d
Hedygh <dyouahes@gmail.com>
2025-11-05 12:33:37 +0100

    5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,15 +12,17 @@ char *_strstr(char *haystack, char *needle)
 	int i = 0;
 	int j = 0;
 
+	if (!needle[0])
+		return (haystack);
 	while (haystack[i])
 	{
 		j = 0;
-			while (haystack[i + j] == needle[j] && needle[j])
-			{
-				if (needle[j + 1] == '\0')
-					return (&haystack[i]);
-				j++;
-			}
+		while (haystack[i + j] == needle[j] && needle[j])
+		{
+			if (needle[j + 1] == '\0')
+				return (&haystack[i]);
+			j++;
+		}
 		i++;
 	}
 	return (0);
