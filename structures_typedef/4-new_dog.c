d97415b8a12154bc85ba17d969050ab0080b3a47
Hedygh <dyouahes@gmail.com>
2025-11-13 21:08:34 +0100

    free

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -66,12 +66,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!newname)
 	{
 		free(newname);
+		free(newdog);
 		return (NULL);
 	}
 	newowner = malloc(sizeof(char) * _strlen(owner) + 1);
 	if (!newowner)
 	{
 		free(newowner);
+		free(newdog);
 		return (NULL);
 	}
 	_strcpy(newname, name);
