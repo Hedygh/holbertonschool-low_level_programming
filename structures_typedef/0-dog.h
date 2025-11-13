0bfa68e486e1701894695d00076c4de9fa902c0f
Hedygh <dyouahes@gmail.com>
2025-11-13 12:21:38 +0100

    0

---
+++ structures_typedef/0-dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct deg - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
