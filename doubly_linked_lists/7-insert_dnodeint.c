39685da3874e18eaf01a8d0bc0c85972a3589eac
Hedygh <dyouahes@gmail.com>
2025-12-03 12:32:22 +0100

    handle NULL condition

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,24 +9,26 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 1;
+	unsigned int i = 0;
 	dlistint_t *new;
 	dlistint_t *tmp;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
+	if (!*h)
+		return (NULL);
 	tmp = *h;
 
-	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
-	while (i < idx)
+	while (i < idx - 1) /* stop before the idx */
 	{
 		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
 		i++;
 	}
+	if (tmp == NULL) /* idx out of range */
+		return (NULL);
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
